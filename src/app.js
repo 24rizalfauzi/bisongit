27a77421126b4a55e98d635f08fdf2b79f0e67ee
rzlfauzi <xsis.rizalf@xl.co.id>
2020-04-20 20:46:06 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,94 @@
+'use strict';
+
+global.config = require('./config.js')
+
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+init()
+
+//pull code
+var CronJob = require('cron').CronJob;
+var job = new CronJob('0 45 1 * * *', async function() {
+    await pullscheduler()
+    await pullweb()
+    await pullmiddleware()
+}, null, true, 'Asia/Jakarta')
+job.start()
+
+async function init(){
+    await pullscheduler()
+    await pullweb()
+    await pullmiddleware()
+}
+
+async function pullscheduler(){
+    var command = `cd "${config.dirbisonscheduler}"`+
+                    `&&git init`+
+                    `&&git remote set-url origin https://github.com/24rizalfauzi/bisonscheduler.git`+
+                    `&&git reset --hard origin/master`+
+                    `&&${config.gitpullbisonscheduler}`+
+                    `&&copy /y "${config.dirbisongit}\\src\\template_config\\bisonscheduler\\config.js" "${config.dirbisonscheduler}\\src\\config.js"`
+    console.log(command)
+    var execmiddleware = exec(command, function(err, stdout, stderr) {
+        if (err) {
+            // should have err.code here?  
+            console.log(err)
+        }
+        console.log('stdout pullscheduler :');
+        console.log(stdout);
+    });
+
+    execmiddleware.on('exit', function (code) {
+        // exit code is code
+        console.log('code pullscheduler :');
+        console.log(code)
+    });
+}
+
+async function pullweb(){
+    var command = `cd "${config.dirbisonweb}"`+
+                    `&&git init`+
+                    `&&git remote set-url origin https://github.com/24rizalfauzi/bisonweb.git`+
+                    `&&git reset --hard origin/master`+
+                    `&&${config.gitpullbisonweb}`
+    console.log(command)
+    var execweb = exec(command, function(err, stdout, stderr) {
+        if (err) {
+            // should have err.code here?  
+            console.log(err)
+        }
+        console.log('stdout pullweb :');
+        console.log(stdout);
+    });
+
+    execweb.on('exit', function (code) {
+        // exit code is code
+        console.log('code pullweb :')
+        console.log(code)
+    });
+}
+
+async function pullmiddleware(){
+    var command = `cd "${config.dirbisonmiddleware}"`+
+                    `&&git init`+
+                    `&&git remote set-url origin https://github.com/24rizalfauzi/bisonmiddleware.git`+
+                    `&&git reset --hard origin/master`+
+                    `&&${config.gitpullbisonmiddleware}`+
+                    `&&copy /y "${config.dirbisongit}\\src\\template_config\\bisonmiddleware\\config.js" "${config.dirbisonmiddleware}\\src\\config.js"`
+    console.log(command)
+    var execmiddleware = exec(command, function(err, stdout, stderr) {
+        if (err) {
+            // should have err.code here?  
+            console.log(err)
+        }
+        console.log('stdout pullmiddleware :');
+        console.log(stdout);
+    });
+
+    execmiddleware.on('exit', function (code) {
+        // exit code is code
+        console.log('code pullmiddleware :');
+        console.log(code)
+    });
+}
---
+++ src/config.js
@@ -0,0 +1,17 @@
+'use strict'
+//GIT
+var dirbisongit				= 'E:\\EP\\BISONDOCS\\github\\dev\\bisongit' // tempat folder middleware di server
+var dirbisonmiddleware		= 'E:\\EP\\BISONDOCS\\github\\prod\\bison_middleware' // tempat folder middleware di server
+var dirbisonscheduler		= 'E:\\EP\\BISONDOCS\\github\\prod\\bison_scheduler' // tempat folder scheduler di server
+var dirbisonweb				= 'E:\\xampp\\htdocs\\bisonweb' //bisonweb yang ada di htdocs
+var gitpullbisonmiddleware	= 'git pull https://24rizalfauzi:*****r1zalf4Uzi*****@github.com/24rizalfauzi/bisonmiddleware.git' //buat pull otomatis
+var gitpullbisonweb			= 'git pull https://24rizalfauzi:*****r1zalf4Uzi*****@github.com/24rizalfauzi/bisonweb.git' //buat pull otomatis
+var gitpullbisonscheduler	= 'git pull https://24rizalfauzi:*****r1zalf4Uzi*****@github.com/24rizalfauzi/bisonscheduler.git' //buat pull otomatis
+
+module.exports.dirbisongit		= dirbisongit
+module.exports.dirbisonmiddleware		= dirbisonmiddleware
+module.exports.dirbisonweb				= dirbisonweb
+module.exports.dirbisonscheduler		= dirbisonscheduler
+module.exports.gitpullbisonweb			= gitpullbisonweb
+module.exports.gitpullbisonmiddleware	= gitpullbisonmiddleware
+module.exports.gitpullbisonscheduler	= gitpullbisonscheduler
